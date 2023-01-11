cff9129dae27dcc00a65e32877c5b01024343066
Stepan-Ilin <110720937+Stepan-Ilin@users.noreply.github.com>
2023-01-11 23:38:27 +0300

    BinarySearch.py done fix

--- Python/BinarySearch.py
+++ Python/BinarySearch.py
@@ -1,19 +1,21 @@
-lis = [1, 28, 34, 45, 47, 58, 59, 62, 74, 82, 87, 90]
+lis = [1, 28, 34, 45, 58, 58, 58, 62, 74, 82, 87, 90]
 def binary_search (ar, num):
 
     _min = 0
     _max = ar.__len__() - 1
+    if ar[0] == num: return _min + 1
+    if ar[_max] == num: return _max + 1
     while  (_max - _min) != 1:
        i = _min + (_max - _min)//2
        if ar[i] == num:
-           return str(i)
+           return str(i+1)
        elif ar[i] < num:
            _min = i
        elif ar[i] > num:
            _max = i
     return "null"
 
-print(binary_search(lis, 83))
+print(binary_search(lis, 58))
 
 
 
