53ab1bba7226277c35308360a983af32f13b614c
Stepan-Ilin <110720937+Stepan-Ilin@users.noreply.github.com>
2023-01-11 22:20:24 +0300

    BinarySearch.py done

--- Python/BinarySearch.py
+++ Python/BinarySearch.py
@@ -1,16 +1,19 @@
 lis = [1, 28, 34, 45, 47, 58, 59, 62, 74, 82, 87, 90]
 def binary_search (ar, num):
-    n=2
-    min=0
-    max=ar.__len__()
 
-    i = max-min//2
-    if ar[i] == num : return i
-    elif ar[i] < num : min = i
-    elif ar[i] > num: max = i
-    return b
+    _min = 0
+    _max = ar.__len__() - 1
+    while  (_max - _min) != 1:
+       i = _min + (_max - _min)//2
+       if ar[i] == num:
+           return str(i)
+       elif ar[i] < num:
+           _min = i
+       elif ar[i] > num:
+           _max = i
+    return "null"
 
-print(binary_search(lis))
+print(binary_search(lis, 83))
 
 
 
--- Python/Teras.py
+++ Python/Teras.py
@@ -1,6 +1,7 @@
 import pytesseract
+
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-#custom_config = r'--oem 3 --psm 13'
+# custom_config = r'--oem 3 --psm 13'
 import cv2
 import pyscreenshot as ps
 
@@ -8,31 +9,31 @@ import numpy
 from mss import mss
 from PIL import Image
 from ahk import AHK
+
 ahk = AHK()
 
 win = ahk.win_get(title='Asterios')
 win.activate()
-#img = Image.open("C:\py.projects\objects_la2\ext.png")
+# img = Image.open("C:\py.projects\objects_la2\ext.png")
 from pyscreenshot.plugins.msswrap import sct
 
+# img = cv2.imread("C:\py.projects\objects_la2\\ty777777.png",cv2.IMREAD_GRAYSCALE)
+# cv2.imwrite("C:\py.projects\objects_la2\\ry6666666.png", ps.grab())
 
-#img = cv2.imread("C:\py.projects\objects_la2\\ty777777.png",cv2.IMREAD_GRAYSCALE)
-#cv2.imwrite("C:\py.projects\objects_la2\\ry6666666.png", ps.grab())
-
-    # Part of the screen to capture
-box_sorry = {"left": win.rect[0]+22, "width":26, "top": win.rect[1]+390, "height": 12}
+# Part of the screen to capture
+box_sorry = {"left": win.rect[0] + 22, "width": 26, "top": win.rect[1] + 390, "height": 12}
 img_sorry_color = numpy.array(mss().grab(box_sorry))
-img_sorry_gray = cv2.cvtColor(img_sorry_color,cv2.COLOR_BGR2GRAY)
+img_sorry_gray = cv2.cvtColor(img_sorry_color, cv2.COLOR_BGR2GRAY)
 cv2.imshow("OpenCV/Numpy normal", img_sorry_gray)
-text_sorry = pytesseract.image_to_string(img_sorry_gray) #, config=custom_config)
+text_sorry = pytesseract.image_to_string(img_sorry_gray)  # , config=custom_config)
 print(text_sorry)
 
 cv2.waitKey(0)
 
-x=310
-y=510
+x = 310
+y = 510
 
-if text_sorry=='Sorry\n':
+if text_sorry == 'Sorry\n':
     ahk.click(x + win.rect[0] - 274 + 10, y + win.rect[1] + 40 + 165 - 127 - 127 + 34)
     print(ahk.mouse_position)
 cv2.waitKey(0)
